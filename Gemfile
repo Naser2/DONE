2e4b003fb520bdb68827e31bef5ac9add0c05ecd
github username <github email address>
2018-09-17 22:21:18 -0400

    'newlyposted && about' added bootstrap for fonts

--- Gemfile
+++ Gemfile
@@ -29,7 +29,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
+#adding http Party
+gem 'httparty'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
+  get 'home/about'
+  get 'home/newlyposted'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
